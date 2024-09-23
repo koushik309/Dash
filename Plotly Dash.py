578540904fa2e8628be16eb64f605d833ba9cc96
koushik309 <koushikvarma9999@gmail.com>
2024-09-23 22:52:21 +0200

    scatter Plot

--- Plotly Dash.py
+++ Plotly Dash.py
@@ -1,46 +1,68 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc, html
+import plotly.graph_objs as go
+import numpy as np
 
-app = dash.Dash()
+app = dash.Dash(__name__)
+
+np.random.seed(50)
+x_rand = np.random.randint(1, 61, 60)
+y_rand = np.random.randint(1, 61, 60)
 
 colors = {
     'text': '#ff0000',
-    'plot_color':'#D3D3D3',
-    'paper_color':'#00FFFF'
+    'plot_color': '#D3D3D3',
+    'paper_color': '#00FFFF'
 }
 
 app.layout = html.Div([
-    html.H1(children = 'Hello Dash!!!',
-            style ={
-                'textAlign': 'center',
-                'color': colors['text'] #RED
-            }
-            ),
-    html.Div(children = 'Dash - A data product developement framework from display',
-             style ={
+    html.H1(children='Hello Dash!!!',
+            style={
                 'textAlign': 'center',
                 'color': colors['text']
             }
             ),
+    html.Div(children='Dash - A data product development framework from display',
+             style={
+                 'textAlign': 'center',
+                 'color': colors['text']
+             }
+             ),
     dcc.Graph(
-        id = 'sample-graph',
+        id='sample-graph',
         figure={
             'data': [
-                {'x':[4,6,8],'y':[12,16,18],'type':'bar','name': 'first chart'},
-                {'x':[4,5,6],'y':[20,24,26],'type':'bar','name': 'second chart'}
+                {'x': [4, 6, 8], 'y': [12, 16, 18], 'type': 'bar', 'name': 'first chart'},
+                {'x': [4, 5, 6], 'y': [20, 24, 26], 'type': 'bar', 'name': 'second chart'}
             ],
             'layout': {
                 'plot_bgcolor': colors['plot_color'],
-                'paper_bgcolor' : colors['paper_color'],
+                'paper_bgcolor': colors['paper_color'],
                 'font': {
-                  'color': colors['text']  
+                    'color': colors['text']
                 },
-                'title': 'simple bar chart'
+                'title': 'Simple Bar Chart'
             }
+        }
+    ),
+    dcc.Graph(
+        id='Scatter_chart',
+        figure={
+            'data': [
+                go.Scatter(
+                    x=x_rand,
+                    y=y_rand,
+                    mode='markers'
+                )
+            ],
+            'layout': go.Layout(
+                title='Scatter plot of random 60 points',
+                xaxis={'title': 'Random X values'},
+                yaxis={'title': 'Random Y values'}
+            )
         }
     )
 ])
 
 if __name__ == '__main__':
-    app.run_server(port= 4050)  
+    app.run_server(port=4050)
