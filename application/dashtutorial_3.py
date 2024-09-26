6ad5bec55f32e0bd2030c5a85cde37ea4b08ec22
koushik309 <koushikvarma9999@gmail.com>
2024-09-26 14:56:55 +0200

    t-3

--- application/dashtutorial_3.py
+++ application/dashtutorial_3.py
@@ -1,13 +1,54 @@
 import dash
-import dash_core_components as dcc
-import dash_html_components as html
+from dash import dcc
+from dash import html
+import yfinance as yf # install yfinance- 'pip install yfinance'
+import datetime
+from dash.dependencies import Input,Output
 
+# Initialize the Dash app
 app = dash.Dash()
 
+# Define the app layout
 app.layout = html.Div(children=[
+    html.Div(
+        children='symbol to graph.'
+    ),
+    dcc.Input(
+    id ='input',
+    value='',
+    type= 'text'
+),
+    html.Div(id='output-graph'),
+   
+]),
+@app.callback(
+    Output(component_id='output-graph', component_property = 'children'),
+    [Input(component_id='input', component_property='value')]
+)
+def update_graph(input_data):
+    # Define the date range
+    start = datetime.datetime(2015, 1, 1)
+    end = datetime.datetime.now()
+    # Fetch data using yfinance
+    df = yf.download(input_data, start=start, end=end)
+    return dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {
+                    'x': df.index,
+                    'y': df['Close'],
+                    'type': 'line',
+                    'name': input_data
+                },
+            ],
+            'layout': {
+                'title': input_data
+            }
+        }
+    )
 
-    
-    ])
 
+# Run the app
 if __name__ == '__main__':
     app.run_server(debug=True)
